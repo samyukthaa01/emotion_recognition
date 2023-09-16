bb6fdfbb299fc8c2c1a105c10f0b7f21a3b553d5
samyukthaa01 <145200371+samyukthaa01@users.noreply.github.com>
2023-09-16 22:38:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import streamlit as st
 # give a title to our app
 st.title("Emotion Recognition Application")
 st.sidebar.header("Choose an Option")
+# Add a radio button to choose between CNN and KNN
+method = st.sidebar.radio("Choose Emotion Recognition Method", ("CNN", "KNN"))
+# Add a radio button to choose between webcam and image upload
+input_type = st.sidebar.radio("Choose Input Type", ("Webcam", "Upload Image"))
 
 
 
