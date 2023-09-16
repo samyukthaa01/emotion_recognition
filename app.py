7132756211013723530bfd983de9126d34212b20
samyukthaa01 <145200371+samyukthaa01@users.noreply.github.com>
2023-09-16 22:39:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ st.sidebar.header("Choose an Option")
 method = st.sidebar.radio("Choose Emotion Recognition Method", ("CNN", "KNN"))
 # Add a radio button to choose between webcam and image upload
 input_type = st.sidebar.radio("Choose Input Type", ("Webcam", "Upload Image"))
+# Based on user selections, perform the chosen action
+if method == "CNN":
+    st.write("You selected Emotion Recognition with CNN.")
+# You can add code here to run CNN-based emotion recognition.
+
 
 
 
