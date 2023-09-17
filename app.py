f202654cb61abca6ef0659478bc5b559009afb7d
samyukthaa01 <145200371+samyukthaa01@users.noreply.github.com>
2023-09-17 12:00:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 import numpy as np
 from PIL import Image
 import tensorflow as tf
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
+
+# Set the start method to 'spawn' or 'forkserver'
+#mp.set_start_method('spawn')#
+
+import tensorflow as tf
+from tensorflow.keras import optimizers
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dropout, BatchNormalization, LeakyReLU, Activation
+from tensorflow.keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from keras.utils import np_utils
+import scikitplot
+import seaborn as sns
+from matplotlib import pyplot
 import json
 import keras
+import math
 import os
 
 # Function to load and inspect model JSON
